629471a260598a0643faa3701c08e757d88d5913
Stephen Wright <wrighs6@rpi.edu>
2024-05-29 13:04:40 -0400

    serve json

--- api/main.go
+++ api/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-  "fmt"
+  "encoding/json"
   "net/http"
 )
 
@@ -9,7 +9,8 @@ func main() {
   mux := http.NewServeMux()
 
   mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode([]string{"Team 1", "Team 2", "Team 3"})
   })
 
   http.ListenAndServe(":80", mux)
