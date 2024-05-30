ab53d5a62ee4716de6bcf82ec1205175f185ef52
Stephen Wright <wrighs6@rpi.edu>
2024-05-30 10:58:06 -0400

    tabs to spaces

--- api/main.go
+++ api/main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"net/http"
+  "encoding/json"
+  "log"
+  "net/http"
 )
 
 func main() {
-	mux := http.NewServeMux()
+  mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode([]string{"Team 1", "Team 2", "Team 3"})
-		if r.URL.Path != "/" {
-			http.NotFound(w, r)
-			return
-		}
-		if r.Method != "GET" {
-			http.Error(w, "method is not supported", http.StatusNotFound)
-			return
-		}
-	})
+  mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode([]string{"Team 1", "Team 2", "Team 3"})
+    if r.URL.Path != "/" {
+      http.NotFound(w, r)
+      return
+    }
+    if r.Method != "GET" {
+      http.Error(w, "method is not supported", http.StatusNotFound)
+      return
+    }
+  })
 
-	if err := http.ListenAndServe(":80", mux); err != nil {
-		log.Fatal(err)
-	}
+  if err := http.ListenAndServe(":80", mux); err != nil {
+    log.Fatal(err)
+  }
 }
