9c25ee4f55d147d01d880a12ba01b890b9a8ef60
Stephen Wright <wrighs6@rpi.edu>
2024-06-08 12:52:44 -0400

    connect api to database

--- api/main.go
+++ api/main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	// Create a new client and connect to the db container
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("DB_CONN")))
+	if err != nil {
+		panic(err)
+	}
+
+	// Send a ping to confirm a successful connection
+	var result bson.M
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
 	mux := http.NewServeMux()
 
 	tasks := getTasks()
