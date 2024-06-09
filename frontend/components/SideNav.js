3e6ed4970aff02e3f86970a17f2a02269775d18a
Stephen Wright <wrighs6@rpi.edu>
2024-06-09 10:17:14 -0400

    use api for list of teams

--- frontend/components/SideNav.js
+++ frontend/components/SideNav.js
@@ -1,17 +1,24 @@
 import { html } from 'htm/preact';
+import { useState, useEffect } from 'preact/hooks';
 
 export default function SideNav() {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(async () => {
+    const response = await fetch(`https://api.${window.location.host}/teams`);
+    const data = await response.json();
+    setTeams(data);
+  }, []);
+
   return html`
     <nav>
       <ul>
-        <li><a href="#">My Tasks</a></li>
+        <li><button>My Tasks</button></li>
         <li><hr /></li>
-        <li><a href="#">Team 1</a></li>
-        <li><a href="#">Team 2</a></li>
-        <li><a href="#">Team 3</a></li>
+        ${teams.map(team => html`<li><button>${team.name}</button></li>`)}
         <li><hr /></li>
-        <li><a href="#">Settings</a></li>
-        <li><a href="#">Logout</a></li>
+        <li><button>Settings</button></li>
+        <li><button>Logout</button></li>
       </ul>
     </nav>
   `;
