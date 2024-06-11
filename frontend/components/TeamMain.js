0a905083de468e4921ac5d6fa22ba7544132acba
Aeonoi <dzhou527@gmail.com>
2024-06-11 14:20:33 -0400

    Added tasks

--- frontend/components/TeamMain.js
+++ frontend/components/TeamMain.js
@@ -1,9 +1,19 @@
-import { html } from 'htm/preact';
+import { html } from "htm/preact";
+import { useState, useEffect } from "preact/hooks";
 
 export default function TeamMain() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(async () => {
+    const response = await fetch(`https://api.${window.location.host}/tasks`);
+    const data = await response.json();
+    setTasks(data);
+    console.log(data);
+  }, []);
+
   return html`
     <main>
-      <p>Tasks go here.</p>
+      ${tasks.map((task) => html`<div class="task-row">${task.name}</div>`)}
     </main>
   `;
 }
