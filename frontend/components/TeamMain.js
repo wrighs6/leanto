2fa00a6c96915b48e46a41d41d9a8093c8bbc4e5
Stephen Wright <wrighs6@rpi.edu>
2024-08-02 14:07:16 -0400

    update task form to use new components

--- frontend/components/TeamMain.js
+++ frontend/components/TeamMain.js
@@ -1,25 +1,10 @@
 import { html } from "htm/preact";
 import { useState, useEffect } from "preact/hooks";
+import TaskForm from "./TaskForm.js";
 
-async function taskPostJSON(data) {
-  try {
-    const response = await fetch(`https://api.${window.location.host}/tasks`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: data,
-    });
-    const result = await response.json();
-    console.log("Success:", result);
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-
-function getTasks(props, tasks) {
+function getTasks(selected, tasks) {
   // get all of the tasks from all of the team for the user
-  if (props.taskState == "") {
+  if (selected == "") {
     const allTasks = tasks.map(
       (task) =>
         html`<div class="task-row border">
@@ -35,7 +20,7 @@ function getTasks(props, tasks) {
   // get all of the tasks for the specified state, i.e. Team 1
   else {
     const teamTasks = tasks.map((task) => {
-      if (task.team.id == props.taskState) {
+      if (task.team.id == selected) {
         return html`<div class="task-row border">
           <div class="section">${task.name}</div>
           <div class="section">${task.assignedTo}</div>
@@ -49,44 +34,16 @@ function getTasks(props, tasks) {
   }
 }
 
-export default function TeamMain(props) {
+export default function TeamMain({selected}) {
   const [tasks, setTasks] = useState([]);
   const [refresh, setRefresh] = useState(false);
+  const [formOpen, setFormOpen] = useState(false);
 
   useEffect(async () => {
     const response = await fetch(`https://api.${window.location.host}/tasks`);
     const data = await response.json();
     setTasks(data);
   }, [refresh]);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const data = new FormData(event.target);
-
-    var object = {};
-    //data.forEach((value, key) => (object[key] = value));
-    for (let [key, value] of data) {
-      // convert Date() types properly
-      if (key == "dueDate" && value != "") {
-        object[key] = new Date(value);
-      }
-      // add field if not empty
-      else if (key != "dueDate" && value != "") {
-        // special case for assignedTo as it is a list
-        if (key == "assignedTo") {
-          object[key] = data.getAll("assignedTo");
-        } else {
-          object[key] = value;
-        }
-      }
-    }
-    object.team = props.taskState;
-    var json = JSON.stringify(object);
-    taskPostJSON(json);
-    document.getElementById("add-task").hidePopover();
-    setTimeout(() => setRefresh(!refresh), 1000);
-  };
 
   return html`
     <main>
@@ -95,76 +52,11 @@ export default function TeamMain(props) {
         <button class="left-button button">Filter</button>
       </div>
       <div class="add-task">
-        <button popovertarget="add-task" class="right-button button">
+        <button class="right-button button" onClick=${() => setFormOpen(true)}>
           Add Task
         </button>
-
-        <div id="add-task" popover="manual">
-          <button
-            class="close-button"
-            popovertarget="add-task"
-            popovertargetaction="hide"
-            type="button"
-            aria-label="Close alert"
-          >
-            <span aria-hidden="true">❌</span>
-          </button>
-          <form onSubmit=${handleSubmit}>
-            <ul>
-              <li>
-                <label for="name">Name:</label><br class="extra-margin" />
-                <input type="text" id="name" name="name" /><br />
-              </li>
-              <li>
-                <label for="description">Description:</label
-                ><br class="extra-margin" />
-                <textarea id="description" name="description"></textarea><br />
-              </li>
-              <li>
-                <label for="dueDate">Due Date:</label
-                ><br class="extra-margin" />
-                <input
-                  type="datetime-local"
-                  name="dueDate"
-                  id="DueDate"
-                  aria-describedby="date-format"
-                /><br />
-              </li>
-              <li>
-                <label for="assignedTo">Assigned To:</label
-                ><br class="extra-margin" />
-                <input type="text" id="assignedTo" name="assignedTo" /><br />
-              </li>
-              <li>
-                <label for="priority">Priority:</label
-                ><br class="extra-margin" />
-                <select id="priority" name="priority">
-                  <option label="--Select One--"></option>
-                  <option>Low Priority</option>
-                  <option>Medium Priority</option>
-                  <option>High Priority</option></select
-                ><br />
-              </li>
-              <li>
-                <label for="status">Status:</label><br class="extra-margin" />
-                <select id="status" name="status">
-                  <option label="--Select One--"></option>
-                  <option>Not Started</option>
-                  <option>Started</option>
-                  <option>Done</option></select
-                ><br />
-              </li>
-              <li>
-                <input
-                  type="submit"
-                  value="Add task"
-                  popovertargetaction="hide"
-                />
-              </li>
-            </ul>
-          </form>
-        </div>
       </div>
+      <${TaskForm} open=${formOpen} close=${() => setFormOpen(false)} refresh=${() => setRefresh(!refresh)} selected=${selected} />
       <div class="task-row border">
         <div class="header">Name</div>
         <div class="header">Assigned</div>
@@ -172,7 +64,7 @@ export default function TeamMain(props) {
         <div class="header">Task Priority</div>
         <div class="header">Task Status</div>
       </div>
-      <div id="tasksContainer">${getTasks(props, tasks)}</div>
+      <div id="tasksContainer">${getTasks(selected, tasks)}</div>
     </main>
   `;
 }
